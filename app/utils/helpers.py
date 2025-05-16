12ac238d7acb2d3618460fff1e7014b6afc35656
Vincent Hu <vincent.hu@outlook.co.nz>
2025-05-16 20:40:54 +1200

    Remove Font Awesome stylesheet links from multiple HTML files

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -2,3 +2,15 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def get_image_extension(mimetype):
+    if not mimetype.startswith('image/'):
+        return None
+
+    ext = mimetype.split('/')[-1].lower()
+    if ext == 'jpeg':
+        return 'jpg'
+    elif ext in ['png', 'gif', 'jpg']:
+        return ext
+    else:
+        return None
